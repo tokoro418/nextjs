d6184c3beded970019c9e90b4e2033fad2c7b04a
tokoro2 <hirofumi.tokorodani@5starclutch.com>
2025-08-31 21:27:55 +0900

    add components

---
+++ src/app/components/sidemenu.tsx
@@ -0,0 +1,9 @@
+import "../globals.css"
+
+export const SideMenu = () => {
+    return (
+        <div className="w-96 bg-white shadow rounde">
+            side           
+        </div>
+    )
+}
